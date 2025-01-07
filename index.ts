fd383539db1f1b6b56819a84f98ca2f6f28e6202
Sofia Perone <sofiaperone62@gmail.com>
2025-01-07 04:45:20 -0300

    borrar?

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import express from "express"
-import sequelize from './api/database.js'; 
+
 
 const app = express();
 
 app.use (express.json())
 
-sequelize.authenticate();
 
 app.get ( "/" , (req,res) => {
     res.status(200).json({
